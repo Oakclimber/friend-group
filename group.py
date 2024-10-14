4328d6d15b1565f487d7747f7e882f868c663811
PUNCHY <ucbvzp1@ucl.ac.uk>
2024-10-14 11:50:17 +0100

    version with connection class

--- group.py
+++ group.py
@@ -2,4 +2,47 @@
 
 # Your code to go here...
 
-my_group =
+class person:
+    def __init__(self,name,age,job):
+        self.name = name
+        self.age = age
+        self.job = []
+        self.job.append(job)
+        self.connect=[]
+    def addconnect(self,connect):
+        self.connect.append(connect)
+    def printconnect(self):
+        for i in range(len(self.connect)):
+            if self.connect[i].name1 == self.name:
+                print(self.connect[i].name2+' '+self.connect[i].connect)
+            if self.connect[i].name2 == self.name:
+                print(self.connect[i].name1+' '+self.connect[i].connect)
+
+class connection:
+    def __init__(self,name1,name2,connect):
+        self.name1=name1
+        self.name2=name2
+        self.connect=connect
+
+a = person("Jill", 26, "a biologist")
+b = person("Zalika", 28, "an artist")
+c = person("John", 27, "a writer")
+d = person("Nash", 34, "a chef")
+
+r1=connection("Jill","Zalika","friend")
+r2=connection("Jill","John","partner")
+r3=connection("Nash","John","cousin")
+r4=connection("Nash","Zalika","landlord")
+
+a.addconnect(r1)
+a.addconnect(r2)
+b.addconnect(r1)
+c.addconnect(r2)
+d.addconnect(r3)
+d.addconnect(r4)
+
+a.printconnect()
+b.printconnect()
+c.printconnect()
+d.printconnect()
+
